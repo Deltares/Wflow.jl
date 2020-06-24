6cf99c09c5c6e24a5991aaa12150302a9761598d
verseve <willem.vanverseveld@gmail.com>
2020-06-24 09:24:55 +0200

    reservoirs (simple) - Reservoir struct (Array{Union{Nothing, Main.Wflow.SimpleReservoir{Float64}},1}) and update function. - Reservoir included in routing (surface_flow.jl). - Assumption is made that reservoirs only occur in river cells. - For reservoir cells pits are defined (subsurface flow and overland flow completely contribute to reservoir inflow).   These are not forced onto the dag/ldd but handled in the update functions.

--- src/subsurface_flow.jl
+++ src/subsurface_flow.jl
@@ -14,6 +14,7 @@ Base.@kwdef struct LateralSSF{T}
         ((kh₀ .* βₗ) ./ f) .* (exp.(-f .* zi) - exp.(-f .* soilthickness)) .* dw    # Subsurface flow [mm³ Δt⁻¹]
     ssfmax::Vector{T} = ((kh₀ .* βₗ) ./ f) .* (1.0 .- exp.(-f .* soilthickness))     # Maximum subsurface flow [mm² Δt⁻¹]
     to_river::Vector{T} = zeros(length(f))  # Part of subsurface flow [mm³ Δt⁻¹] that flows to the river
+    pits::Vector{Int64} = zeros(Int64,length(f))
 end
 
 """
@@ -32,11 +33,14 @@ end
 function update(ssf::LateralSSF, dag, toposort, frac_toriver, river)
     for v in toposort
         upstream_nodes = inneighbors(dag, v)
-        if Bool(river[v])
+        if Bool(river[v]) & (ssf.pits[v] == 0)
             ssfin = isempty(upstream_nodes) ? 0.0 :
-                sum(ssf.ssf[i] * (1.0 - frac_toriver[i]) for i in upstream_nodes)
+                sum(ssf.ssf[i] * (1.0 - frac_toriver[i]) for i in upstream_nodes if ssf.pits[i] == 0)
             ssf.to_river[v] = isempty(upstream_nodes) ? 0.0 :
-                sum(ssf.ssf[i] * frac_toriver[i] for i in upstream_nodes)
+                sum(ssf.ssf[i] * frac_toriver[i] for i in upstream_nodes if ssf.pits[i] == 0)
+        elseif Bool(river[v]) & (ssf.pits[v] == 1)
+            ssf.to_river[v] = isempty(upstream_nodes) ? 0.0 : sum(ssf.ssf[i] for i in upstream_nodes)
+            ssfin = 0.0
         else
             ssfin = isempty(upstream_nodes) ? 0.0 : sum(ssf.ssf[i] for i in upstream_nodes)
         end
