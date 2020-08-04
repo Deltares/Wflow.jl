c1ee97a0993ace1be3e1f63d07b6392a4703938d
Martijn Visser <mgvisser@gmail.com>
2020-08-04 18:21:53 +0200

    add run_simulation top level interface

---
+++ test/run.jl
@@ -0,0 +1,10 @@
+# simple example of running a model simulation, not part of the tests
+using Wflow
+
+tomlpath = joinpath(@__DIR__, "config.toml")
+config = Wflow.Config(tomlpath)
+config["model"]["reinit"] = true
+
+model = Wflow.initialize_sbm_model(config)
+@time Wflow.run_simulation(model, config; close_files=true)
+# Wflow.close_files(model)
