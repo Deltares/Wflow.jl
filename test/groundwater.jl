87e34872d3c7df7d941a1c9f4183c7100cb95c4a
Huite Bootsma <huitebootsma@gmail.com>
2020-09-30 17:58:50 +0200

    Update tests, and changes in response: * Move special function into a separate file * Include Polynomials.jl in project toml * Make a start with integration tests * Include initialize_conductance for confined aquifer * Include a stable timestep estimate

--- test/groundwater.jl
+++ test/groundwater.jl
@@ -1,123 +1,17 @@
-# Not all special functions have been implemented yet:
-# https://github.com/JuliaMath/SpecialFunctions.jl/issues/19
-# (MIT licensed)
-# 
-# This expint implementation is copied from:
-# https://github.com/mschauer/Bridge.jl/blob/a719e8a57aaa77fa4ca289c25978a7e080908826/src/expint.jl#L1
-########################################################################
-# Inlined, optimized code for the exponential integral E₁ in double precison
-# by Steven G. Johnson (@stevengj), Code under MIT license.
-# For more explanations, see course notes by Steven G. Johnson at
-#     https://github.com/stevengj/18S096-iap17/blob/master/pset3/pset3-solutions.ipynb
+"""
+    transient_head(x, time, conductivity, specific_yield, aquifer_length, Β)
 
-using Base.MathConstants: eulergamma
+Non-steady flow in an unconfined rectangular aquifer, with Dirichlet h(0, t) = 0
+on the left edge, and a Neumann Boundary Condition (dh/dx = 0) on the right.
+"""
 
-# n coefficients of the Taylor series of E₁(z) + log(z), in type T:
-function E₁_taylor_coefficients(::Type{T}, n::Integer) where T <: Number
-    n < 0 && throw(ArgumentError("$n ≥ 0 is required"))
-    n == 0 && return T[]
-    n == 1 && return T[-eulergamma]
-    # iteratively compute the terms in the series, starting with k=1
-    term::T = 1
-    terms = T[-eulergamma, term]
-    for k in 2:n
-        term = -term * (k - 1) / (k * k)
-        push!(terms, term)
+function transient_head(x, time, conductivity, specific_yield, aquifer_length, Β)
+    function initial_head(x)
+        2 * √x
     end
-    return terms
+    initial_head(x) / 1.0 + (Β * conductivity * initial_head(aquifer_length) * time) / (specific_yield * aquifer_length * aquifer_length)
 end
 
-# inline the Taylor expansion for a given order n, in double precision
-macro E₁_taylor64(z, n::Integer)
-    c = E₁_taylor_coefficients(Float64, n)
-    taylor = :(@evalpoly zz)
-    append!(taylor.args, c)
-    quote
-        let zz = $(esc(z))
-            $taylor - log(zz)
-        end
-    end
-end
-
-# for numeric-literal coefficients: simplify to a ratio of two polynomials:
-import Polynomials
-# return (p,q): the polynomials p(x) / q(x) corresponding to E₁_cf(x, a...),
-# but without the exp(-x) term
-function E₁_cfpoly(n::Integer, ::Type{T}=BigInt) where T <: Real
-    q = Polynomials.Polynomial(T[1])
-    p = x = Polynomials.Polynomial(T[0,1])
-    for i in n:-1:1
-        p, q = x * p + (1 + i) * q, p # from cf = x + (1+i)/cf = x + (1+i)*q/p
-        p, q = p + i * q, p     # from cf = 1 + i/cf = 1 + i*q/p
-    end
-    # do final 1/(x + inv(cf)) = 1/(x + q/p) = p/(x*p + q)
-    return p, x * p + q
-end
-
-macro E₁_cf64(z, n::Integer)
-    p, q = E₁_cfpoly(n, BigInt)
-    num_expr =  :(@evalpoly zz)
-    append!(num_expr.args, Float64.(Polynomials.coeffs(p)))
-    den_expr = :(@evalpoly zz)
-    append!(den_expr.args, Float64.(Polynomials.coeffs(q)))
-    quote
-        let zz = $(esc(z))
-            exp(-zz) * $num_expr / $den_expr
-        end
-    end
-end
-
-# exponential integral function E₁(z)
-function expint(z::Union{Float64,Complex{Float64}})
-    x² = real(z)^2
-    y² = imag(z)^2
-    if real(z) > 0 && x² + 0.233 * y² ≥ 7.84 # use cf expansion, ≤ 30 terms
-        if (x² ≥ 546121) & (real(z) > 0) # underflow
-            return zero(z)
-        elseif x² + 0.401 * y² ≥ 58.0 # ≤ 15 terms
-            if x² + 0.649 * y² ≥ 540.0 # ≤ 8 terms
-                x² + y² ≥ 4e4 && return @E₁_cf64 z 4
-                return @E₁_cf64 z 8
-            end
-            return @E₁_cf64 z 15
-        end
-        return @E₁_cf64 z 30
-    else # use Taylor expansion, ≤ 37 terms
-        r² = x² + y²
-        return r² ≤ 0.36 ? (r² ≤ 2.8e-3 ? (r² ≤ 2e-7 ? @E₁_taylor64(z,4) :
-                                                        @E₁_taylor64(z,8)) :
-                                                        @E₁_taylor64(z,15)) :
-                                                        @E₁_taylor64(z,37)
-    end
-end
-expint(z::Union{T,Complex{T},Rational{T},Complex{Rational{T}}}) where {T <: Integer} = expint(float(z))
-
-######################################################################
-# exponential integral Eₙ(z)
-
-function expint(n::Integer, z)
-    if n == 1
-        return expint(z)
-    elseif n < 1
-        # backwards recurrence from E₀ = e⁻ᶻ/z
-        zinv = inv(z)
-        e⁻ᶻ = exp(-z)
-        Eᵢ = zinv * e⁻ᶻ
-        for i in 1:-n
-            Eᵢ = zinv * (e⁻ᶻ + i * Eᵢ)
-        end
-        return Eᵢ
-    elseif n > 1
-        # forwards recurrence from E₁
-        e⁻ᶻ = exp(-z)
-        Eᵢ = expint(z)
-        Eᵢ *= !isinf(Eᵢ)
-        for i in 2:n
-            Eᵢ = (e⁻ᶻ - z * Eᵢ) / (i - 1)
-        end
-        return Eᵢ
-    end
-end
 
 """
     drawdown_theis(distance, time, discharge, transmissivity, storativity)
@@ -128,13 +22,6 @@ function drawdown_theis(distance, time, discharge, transmissivity, storativity)
     u = (storativity * distance^2) / (4 * transmissivity * time)
     return discharge / (4 * π  * transmissivity) * expint(1, u)
 end
-
-
-# Avoid 0.0, since it's a singularity
-# X = -9995.0:10.0:9995.0
-# Y = -9995.0:10.0:9995.0
-# time = 0.0:100.0:10_000.0
-# ϕ = [-drawdown_theis(√(x^2 + y^2), t, discharge, kD, S) for t in time, x in X, y in Y]
 
 
 function homogenous_aquifer(nrow, ncol)
@@ -153,8 +40,8 @@ function homogenous_aquifer(nrow, ncol)
         fill(10.0, ncell),  # top
         fill(0.0, ncell),  # bottom
         fill(100.0, ncell),  # area
-        fill(1.0e-5, ncell), # specific storage
-        fill(1.0e-4, ncell),  # storativity 
+        fill(0.1, ncell), # specific storage
+        fill(1.0, ncell),  # storativity 
         fill(0.0, connectivity.nconnection),  # conductance
     )
     unconf_aqf = Wflow.UnconfinedAquifer(
@@ -172,9 +59,6 @@ end
 @testset "groundwater" begin
     ncol = 2
     nrow = 3
-    # TODO: this seems weird to me, I guess x is firstdim due to column major?
-    # which happens because of spatial raster layout?
-    # relates to how active_indices works
     shape = (ncol, nrow)
     Δx = [10.0, 20.0]
     Δy = [5.0, 15.0, 25.0]
@@ -302,6 +186,10 @@ end
             @test Wflow.conductance(unconf_aqf, connectivity, 2, 3, 3) > 0.0   # computes a value based on head
             @test Wflow.conductance(unconf_aqf, connectivity, 1, 2, 1) == 0.0
         end
+        
+        @testset "stable_timestep" begin
+            @test Wflow.stable_timestep(conf_aqf) == 0.25
+        end
 
         @testset "flux-confined" begin
             Q = zeros(3)
@@ -361,22 +249,95 @@ end
         end
     end
     
-    # @testset "integration: flow 1D" begin
-    #    connectivity, aquifer = homogenous_aquifer(3, 1)
+    @testset "integration: steady 1D" begin
+        connectivity, aquifer, _ = homogenous_aquifer(3, 1)
+        constanthead = Wflow.ConstantHead([2.0, 4.0], [1, 3])
+        gwf = Wflow.GroundwaterFlow(
+           aquifer,
+           connectivity,
+           constanthead,
+           Wflow.AquiferBoundaryCondition[],
+        )
+        # Initialize conductance for confined aquifer
+        Wflow.initialize_conductance!(gwf.aquifer, gwf.connectivity)
+        # Set constant head (dirichlet) boundaries
+        gwf.aquifer.head[gwf.constanthead.index] .= gwf.constanthead.head
+
+        Q = zeros(3)    
+        Δt = 0.25 # days
+        for _ in 1:50
+            Wflow.update(gwf, Q, Δt)
+        end
+
+        @test gwf.aquifer.head ≈ [2.0, 3.0, 4.0]
+    end
+
+    @testset "integration: transient 1D" begin
+        nrow = 1
+        ncol = 50
+        shape = (nrow, ncol)
+        conductivity = 10.0
+        top = 50.0
+        bottom = 0.0
+        specific_yield = 0.15
+        cellsize = 10.0
+        aquifer_length = cellsize * ncol 
+
+        # Domain, geometry
+        domain = ones(Bool, shape)
+        Δx = fill(cellsize, ncol)
+        Δy = fill(cellsize, nrow)
+        indices, reverse_indices = Wflow.active_indices(domain, false)
+        connectivity = Wflow.Connectivity(indices, reverse_indices, Δx, Δy)
+        ncell = connectivity.ncell
+        xc = collect(range(5.0, stop=495.0, step=10.0))
+        aquifer = Wflow.UnconfinedAquifer(
+            .√xc,  # initial head
+            fill(conductivity, ncell),
+            fill(top, ncell),
+            fill(bottom, ncell),
+            fill(cellsize * cellsize, ncell),
+            fill(specific_yield, ncell),
+        )
+        # constant head on left boundary, 0 at 0
+        constanthead = Wflow.ConstantHead([√xc[1]], [1])
+        gwf = Wflow.GroundwaterFlow(
+           aquifer,
+           connectivity,
+           constanthead,
+           Wflow.AquiferBoundaryCondition[],
+        )
+
+        Δt = 0.1
+        Q = zeros(50)
+        for _ in 1:10
+            Wflow.update(gwf, Q, Δt)
+        end
+
+        ϕ_analytical = [transient_head(x, 20.0, conductivity, specific_yield, aquifer_length, 1.12) for x in xc]
+    end
+
+    @testset "integration: radialflow 2D" begin
+    #    connectivity, aquifer, _ = homogenous_aquifer(50, 50)
     #    constanthead = Wflow.ConstantHead([5.0, 10.0], [1, 3])
     #    gwf = Wflow.GroundwaterFlow(
-    #        aquifer,
-    #        connectivity,
-    #        constanthead,
-    #        [],
-    #    )
-    # 
-    #    # A hundred timesteps
+    #       aquifer,
+    #       connectivity,
+    #       constanthead,
+    #       Wflow.AquiferBoundaryCondition[],
+    #   )
+# 
+    #   # A hundred timesteps
     #    Δt = 0.5  # days
     #    for _ in 1:100
-    #        Wflow.update(gwf, Δt)
+    #        Wflow.update(gwf, Q, Δt)
     #    end
-    # 
-    #    @test gwf.head ≈ [5.0 7.5 10.0]
-    # end
+# 
+    #   # Compare with analytical solution
+    #   # Avoid 0.0, since it's a singularity
+    #   # X = -9995.0:10.0:9995.0
+    #   # Y = -9995.0:10.0:9995.0
+    #   # time = 0.0:100.0:10_000.0
+    #   # ϕ = [-drawdown_theis(√(x^2 + y^2), t, discharge, kD, S) for t in time, x in X, y in Y]
+    end
 end
---
+++ test/testing_utils.jl
@@ -0,0 +1,120 @@
+# Not all special functions have been implemented yet:
+# https://github.com/JuliaMath/SpecialFunctions.jl/issues/19
+# (MIT licensed)
+# 
+# This expint implementation is copied from:
+# https://github.com/mschauer/Bridge.jl/blob/a719e8a57aaa77fa4ca289c25978a7e080908826/src/expint.jl#L1
+########################################################################
+# Inlined, optimized code for the exponential integral E₁ in double precison
+# by Steven G. Johnson (@stevengj), Code under MIT license.
+# For more explanations, see course notes by Steven G. Johnson at
+#     https://github.com/stevengj/18S096-iap17/blob/master/pset3/pset3-solutions.ipynb
+
+using Base.MathConstants: eulergamma
+
+# n coefficients of the Taylor series of E₁(z) + log(z), in type T:
+function E₁_taylor_coefficients(::Type{T}, n::Integer) where T <: Number
+    n < 0 && throw(ArgumentError("$n ≥ 0 is required"))
+    n == 0 && return T[]
+    n == 1 && return T[-eulergamma]
+    # iteratively compute the terms in the series, starting with k=1
+    term::T = 1
+    terms = T[-eulergamma, term]
+    for k in 2:n
+        term = -term * (k - 1) / (k * k)
+        push!(terms, term)
+    end
+    return terms
+end
+
+# inline the Taylor expansion for a given order n, in double precision
+macro E₁_taylor64(z, n::Integer)
+    c = E₁_taylor_coefficients(Float64, n)
+    taylor = :(@evalpoly zz)
+    append!(taylor.args, c)
+    quote
+        let zz = $(esc(z))
+            $taylor - log(zz)
+        end
+    end
+end
+
+# for numeric-literal coefficients: simplify to a ratio of two polynomials:
+import Polynomials
+# return (p,q): the polynomials p(x) / q(x) corresponding to E₁_cf(x, a...),
+# but without the exp(-x) term
+function E₁_cfpoly(n::Integer, ::Type{T}=BigInt) where T <: Real
+    q = Polynomials.Polynomial(T[1])
+    p = x = Polynomials.Polynomial(T[0,1])
+    for i in n:-1:1
+        p, q = x * p + (1 + i) * q, p # from cf = x + (1+i)/cf = x + (1+i)*q/p
+        p, q = p + i * q, p     # from cf = 1 + i/cf = 1 + i*q/p
+    end
+    # do final 1/(x + inv(cf)) = 1/(x + q/p) = p/(x*p + q)
+    return p, x * p + q
+end
+
+macro E₁_cf64(z, n::Integer)
+    p, q = E₁_cfpoly(n, BigInt)
+    num_expr =  :(@evalpoly zz)
+    append!(num_expr.args, Float64.(Polynomials.coeffs(p)))
+    den_expr = :(@evalpoly zz)
+    append!(den_expr.args, Float64.(Polynomials.coeffs(q)))
+    quote
+        let zz = $(esc(z))
+            exp(-zz) * $num_expr / $den_expr
+        end
+    end
+end
+
+# exponential integral function E₁(z)
+function expint(z::Union{Float64,Complex{Float64}})
+    x² = real(z)^2
+    y² = imag(z)^2
+    if real(z) > 0 && x² + 0.233 * y² ≥ 7.84 # use cf expansion, ≤ 30 terms
+        if (x² ≥ 546121) & (real(z) > 0) # underflow
+            return zero(z)
+        elseif x² + 0.401 * y² ≥ 58.0 # ≤ 15 terms
+            if x² + 0.649 * y² ≥ 540.0 # ≤ 8 terms
+                x² + y² ≥ 4e4 && return @E₁_cf64 z 4
+                return @E₁_cf64 z 8
+            end
+            return @E₁_cf64 z 15
+        end
+        return @E₁_cf64 z 30
+    else # use Taylor expansion, ≤ 37 terms
+        r² = x² + y²
+        return r² ≤ 0.36 ? (r² ≤ 2.8e-3 ? (r² ≤ 2e-7 ? @E₁_taylor64(z,4) :
+                                                        @E₁_taylor64(z,8)) :
+                                                        @E₁_taylor64(z,15)) :
+                                                        @E₁_taylor64(z,37)
+    end
+end
+expint(z::Union{T,Complex{T},Rational{T},Complex{Rational{T}}}) where {T <: Integer} = expint(float(z))
+
+######################################################################
+# exponential integral Eₙ(z)
+
+function expint(n::Integer, z)
+    if n == 1
+        return expint(z)
+    elseif n < 1
+        # backwards recurrence from E₀ = e⁻ᶻ/z
+        zinv = inv(z)
+        e⁻ᶻ = exp(-z)
+        Eᵢ = zinv * e⁻ᶻ
+        for i in 1:-n
+            Eᵢ = zinv * (e⁻ᶻ + i * Eᵢ)
+        end
+        return Eᵢ
+    elseif n > 1
+        # forwards recurrence from E₁
+        e⁻ᶻ = exp(-z)
+        Eᵢ = expint(z)
+        Eᵢ *= !isinf(Eᵢ)
+        for i in 2:n
+            Eᵢ = (e⁻ᶻ - z * Eᵢ) / (i - 1)
+        end
+        return Eᵢ
+    end
+end
