2c9147aee95372c9c26cbb4cc43a2b307f94e6f7
verseve <willem.vanverseveld@gmail.com>
2020-10-16 17:54:12 +0200

    initialization vertical sbm
    moved the initialization of the vertical sbm concept to a separate function. this way it can be used by other models without too much code duplication.

--- src/sbm_model.jl
+++ src/sbm_model.jl
@@ -17,18 +17,10 @@ function initialize_sbm_model(config::Config)
     Δt = Second(config.timestepsecs)
     sizeinmetres = get(config.model, "sizeinmetres", false)
     reinit = get(config.model, "reinit", true)
-    config_thicknesslayers = get(config.model, "thicknesslayers", 0.0)
-    if length(config_thicknesslayers) > 0
-        thicknesslayers = SVector(Tuple(push!(Float64.(config_thicknesslayers), mv)))
-        sumlayers = pushfirst(cumsum(thicknesslayers), 0.0)
-        maxlayers = length(thicknesslayers) # max number of soil layers
-    else
-        maxlayers = 1
-    end
+    do_snow = get(config.model, "snow", false)
     do_reservoirs = get(config.model, "reservoirs", false)
     do_lakes = get(config.model, "lakes", false)
-    do_snow = get(config.model, "snow", false)
-
+    
     nc = NCDataset(static_path)
     dims = dimnames(nc[param(config, "input.subcatchment")])
 
@@ -43,14 +35,7 @@ function initialize_sbm_model(config::Config)
     inds, rev_inds = active_indices(subcatch_2d, missing)
     n = length(inds)
     modelsize_2d = size(subcatch_2d)
-
-    # alt = Wflow.symbols"input.vertical.altitude"
-    # config
-    # ncname = Wflow.param(config, alt, nothing)
-    # nc[ncname]
-
-    altitude =
-        ncread(nc, param(config, "input.vertical.altitude"); sel = inds, type = Float64)
+    
     river_2d = ncread(nc, param(config, "input.river_location"); type = Bool, fill = false)
     river = river_2d[inds]
     riverwidth_2d =
@@ -70,239 +55,6 @@ function initialize_sbm_model(config::Config)
     end
     cellength = abs(mean(diff(x_nc)))
 
-
-    cfmax = ncread(
-        nc,
-        param(config, "cfmax", nothing);
-        sel = inds,
-        defaults = 3.75653,
-        type = Float64,
-    ) .* (Δt / basetimestep)
-    tt = ncread(
-        nc,
-        param(config, "input.vertical.tt", nothing);
-        sel = inds,
-        defaults = 0.0,
-        type = Float64,
-    )
-    tti = ncread(
-        nc,
-        param(config, "input.vertical.tti", nothing);
-        sel = inds,
-        defaults = 1.0,
-        type = Float64,
-    )
-    ttm = ncread(
-        nc,
-        param(config, "input.vertical.ttm", nothing);
-        sel = inds,
-        defaults = 0.0,
-        type = Float64,
-    )
-    whc = ncread(
-        nc,
-        param(config, "input.vertical.whc", nothing);
-        sel = inds,
-        defaults = 0.1,
-        type = Float64,
-    )
-    w_soil = ncread(
-        nc,
-        param(config, "input.vertical.w_soil", nothing);
-        sel = inds,
-        defaults = 0.1125,
-        type = Float64,
-    ) .* (Δt / basetimestep)
-    cf_soil = ncread(
-        nc,
-        param(config, "input.vertical.cf_soil", nothing);
-        sel = inds,
-        defaults = 0.038,
-        type = Float64,
-    )
-
-
-    # soil parameters
-    θₛ = ncread(
-        nc,
-        param(config, "input.vertical.θₛ", nothing);
-        sel = inds,
-        defaults = 0.6,
-        type = Float64,
-    )
-    θᵣ = ncread(
-        nc,
-        param(config, "input.vertical.θᵣ", nothing);
-        sel = inds,
-        defaults = 0.01,
-        type = Float64,
-    )
-    kv₀ = ncread(
-        nc,
-        param(config, "input.vertical.kv₀");
-        sel = inds,
-        defaults = 3000.0,
-        type = Float64,
-    ) .* (Δt / basetimestep)
-    m = ncread(
-        nc,
-        param(config, "input.vertical.m", nothing);
-        sel = inds,
-        defaults = 300.0,
-        type = Float64,
-    )
-    hb = ncread(
-        nc,
-        param(config, "input.vertical.hb", nothing);
-        sel = inds,
-        defaults = 10.0,
-        type = Float64,
-    )
-    soilthickness = ncread(
-        nc,
-        param(config, "input.vertical.soilthickness", nothing);
-        sel = inds,
-        defaults = 2000.0,
-        type = Float64,
-    )
-    infiltcappath = ncread(
-        nc,
-        param(config, "input.vertical.infiltcappath", nothing);
-        sel = inds,
-        defaults = 10.0,
-        type = Float64,
-    )
-    infiltcapsoil = ncread(
-        nc,
-        param(config, "input.vertical.infiltcapsoil", nothing);
-        sel = inds,
-        defaults = 100.0,
-        type = Float64,
-    )
-    maxleakage = ncread(
-        nc,
-        param(config, "input.vertical.maxleakage", nothing);
-        sel = inds,
-        defaults = 0.0,
-        type = Float64,
-    )
-
-    c = ncread(
-        nc,
-        param(config, "input.vertical.c", nothing);
-        sel = inds,
-        defaults = 10.0,
-        type = Float64,
-        dimname = "layer",
-    )
-    kvfrac = ncread(
-        nc,
-        param(config, "input.vertical.khfrac", nothing);
-        sel = inds,
-        defaults = 1.0,
-        type = Float64,
-        dimname = "layer",
-    )
-
-    # fraction open water and compacted area (land cover)
-    waterfrac = ncread(
-        nc,
-        param(config, "input.vertical.waterfrac", nothing);
-        sel = inds,
-        defaults = 0.0,
-        type = Float64,
-    )
-    pathfrac = ncread(
-        nc,
-        param(config, "input.vertical.pathfrac", nothing);
-        sel = inds,
-        defaults = 0.01,
-        type = Float64,
-    )
-
-    # vegetation parameters
-    rootingdepth = ncread(
-        nc,
-        param(config, "input.vertical.rootingdepth", nothing);
-        sel = inds,
-        defaults = 750.0,
-        type = Float64,
-    )
-    rootdistpar = ncread(
-        nc,
-        param(config, "input.vertical.rootdistpar", nothing);
-        sel = inds,
-        defaults = -500.0,
-        type = Float64,
-    )
-    capscale = ncread(
-        nc,
-        param(config, "input.vertical.capscale", nothing);
-        sel = inds,
-        defaults = 100.0,
-        type = Float64,
-    )
-    et_reftopot = ncread(
-        nc,
-        param(config, "input.vertical.et_reftopot", nothing);
-        sel = inds,
-        defaults = 1.0,
-        type = Float64,
-    )
-
-    # if leaf area index climatology provided use sl, swood and kext to calculate cmax, e_r and canopygapfraction
-    if haskey(config.input.vertical, "leaf_area_index")
-       # TODO confirm if leaf area index climatology is present in the NetCDF
-       sl = ncread(
-            nc,
-            param(config, "input.vertical.specific_leaf");
-            sel = inds,
-            type = Float64,
-        )
-        swood = ncread(
-            nc,
-            param(config, "input.vertical.storage_wood");
-            sel = inds,
-            type = Float64,
-        )
-        kext = ncread(nc, param(config, "input.vertical.kext"); sel = inds, type = Float64)
-        cmax = fill(mv, n)
-        e_r = fill(mv, n)
-        canopygapfraction = fill(mv, n)
-    else
-        # cmax, e_r, canopygapfraction only required when leaf area index climatology not provided
-        cmax = ncread(
-            nc,
-            param(config, "input.vertical.cmax", nothing);
-            sel = inds,
-            defaults = 1.0,
-            type = Float64,
-        )
-        e_r = ncread(
-            nc,
-            param(config, "input.vertical.eoverr", nothing);
-            sel = inds,
-            defaults = 0.1,
-            type = Float64,
-        )
-        canopygapfraction = ncread(
-            nc,
-            param(config, "input.vertical.canopygapfraction", nothing);
-            sel = inds,
-            defaults = 0.1,
-            type = Float64,
-        )
-        sl = fill(mv, n)
-        swood = fill(mv, n)
-        kext = fill(mv, n)
-    end
-
-
-    # these are filled in the loop below
-    # TODO see if we can replace this approach
-    nlayers = zeros(Int, n)
-    act_thickl = zeros(Float64, maxlayers, n)
-    s_layers = zeros(Float64, maxlayers + 1, n)
     xl = fill(mv, n)
     yl = fill(mv, n)
     riverfrac = fill(mv, n)
@@ -312,133 +64,9 @@ function initialize_sbm_model(config::Config)
         yl[i] = sizeinmetres ? cellength : lattometres(y[i])[2] * cellength
         riverfrac[i] =
             river[i] ? min((riverlength[i] * riverwidth[i]) / (xl[i] * yl[i]), 1.0) : 0.0
-
-        if length(config_thicknesslayers) > 0
-            act_thickl_, nlayers_ =
-                set_layerthickness(soilthickness[i], sumlayers, thicknesslayers)
-            s_layers_ = pushfirst(cumsum(act_thickl_), 0.0)
-
-            nlayers[i] = nlayers_
-            act_thickl[:, i] = act_thickl_
-            s_layers[:, i] = s_layers_
-        else
-            nlayers[i] = 1
-            act_thickl[:, i] = SVector(soilthickness[i])
-            s_layers[:, i] = pushfirst(cumsum(SVector(soilthickness[i])), 0.0)
-        end
     end
 
-    # needed for derived parameters below
-    act_thickl = svectorscopy(act_thickl, Val{maxlayers}())
-    θₑ = θₛ .- θᵣ
-    soilwatercapacity = soilthickness .* θₑ
-    satwaterdepth = 0.85 .* soilwatercapacity # cold state value for satwaterdepth
-    zi = max.(0.0, soilthickness .- satwaterdepth ./ θₑ) # cold state value for zi
-
-    # copied to array of sarray below
-    vwc = fill(mv, maxlayers, n)
-    vwc_perc = fill(mv, maxlayers, n)
-
-    sbm = SBM{Float64,maxlayers,maxlayers + 1}(
-        Δt = tosecond(Δt),
-        maxlayers = maxlayers,
-        n = n,
-        nlayers = nlayers,
-        n_unsatlayers = fill(0, n),
-        yl = yl,
-        xl = xl,
-        riverfrac = riverfrac,
-        θₛ = θₛ,
-        θᵣ = θᵣ,
-        θₑ = θₑ,
-        kv₀ = kv₀,
-        kvfrac = svectorscopy(kvfrac, Val{maxlayers}()),
-        m = m,
-        hb = hb,
-        soilthickness = soilthickness,
-        act_thickl = act_thickl,
-        sumlayers = svectorscopy(s_layers, Val{maxlayers + 1}()),
-        infiltcappath = infiltcappath,
-        infiltcapsoil = infiltcapsoil,
-        maxleakage = maxleakage,
-        waterfrac = max.(waterfrac .- riverfrac, 0.0),
-        pathfrac = pathfrac,
-        altitude = altitude,
-        rootingdepth = rootingdepth,
-        rootdistpar = rootdistpar,
-        capscale = capscale,
-        et_reftopot = et_reftopot,
-        c = svectorscopy(c, Val{maxlayers}()),
-        stemflow = fill(mv, n),
-        throughfall = fill(mv, n),
-        f = θₑ ./ m,
-        ustorelayerdepth = act_thickl .* 0.0,
-        satwaterdepth = satwaterdepth,
-        zi = zi,
-        soilwatercapacity = soilwatercapacity,
-        canopystorage = fill(0.0, n),
-        cmax = cmax,
-        canopygapfraction = canopygapfraction,
-        e_r = e_r,
-        precipitation = fill(mv, n),
-        temperature = fill(mv, n),
-        potential_evaporation = fill(mv, n),
-        pottrans_soil = fill(mv, n),
-        transpiration = fill(mv, n),
-        ae_ustore = fill(mv, n),
-        ae_sat = fill(mv, n),
-        interception = fill(mv, n),
-        soilevap = fill(mv, n),
-        soilevapsat = fill(mv, n),
-        actcapflux =  fill(mv, n),
-        actevapsat = fill(mv, n),
-        actevap = fill(mv, n),
-        runoff_river = fill(mv, n),
-        runoff_land = fill(mv, n),
-        ae_openw_l = fill(mv, n),
-        ae_openw_r = fill(mv, n),
-        avail_forinfilt = fill(mv, n),
-        actinfilt = fill(mv, n),
-        actinfiltsoil = fill(mv, n),
-        actinfiltpath = fill(mv, n),
-        infiltsoilpath= fill(mv, n),
-        infiltexcess = fill(mv, n),
-        excesswater = fill(mv, n),
-        exfiltsatwater = fill(mv, n),
-        exfiltustore = fill(mv, n),
-        excesswatersoil = fill(mv, n),
-        excesswaterpath = fill(mv, n),
-        runoff = fill(mv, n),
-        net_runoff_river = fill(mv, n),
-        vwc = svectorscopy(vwc, Val{maxlayers}()),
-        vwc_perc = svectorscopy(vwc_perc, Val{maxlayers}()),
-        rootstore = fill(mv, n),
-        vwc_root = fill(mv, n),
-        vwc_percroot = fill(mv, n),
-        ustoredepth = fill(mv, n),
-        transfer = fill(mv, n),
-        recharge = fill(mv, n),
-        actleakage = fill(mv, n),
-        # snow parameters
-        cfmax = cfmax,
-        tt = tt,
-        tti = tti,
-        ttm = ttm,
-        whc = whc,
-        w_soil = w_soil,
-        cf_soil = cf_soil,
-        snow = fill(0.0, n),
-        snowwater = fill(0.0, n),
-        rainfallplusmelt = fill(mv, n),
-        tsoil = fill(10.0, n),
-        # Interception related to climatology (leaf_area_index)
-        sl = sl,
-        swood = swood,
-        kext = kext,
-        leaf_area_index = fill(mv, n),
-        waterlevel_land = fill(mv, n),
-        waterlevel_river = fill(0.0, n), #set to zero to account for cells outside river domain
-    )
+    sbm = initialize_sbm(nc, config, riverfrac, xl, yl, inds)
 
     inds_riv, rev_inds_riv = active_indices(river_2d, 0)
     nriv = length(inds_riv)
@@ -470,7 +98,7 @@ function initialize_sbm_model(config::Config)
     clamp!(βₗ, 0.00001, Inf)
     ldd_2d = ncread(nc, param(config, "input.ldd"); allow_missing = true)
     ldd = ldd_2d[inds]
-    kh₀ = khfrac .* kv₀
+    kh₀ = khfrac .* sbm.kv₀
     dl = fill(mv, n)
     dw = fill(mv, n)
     sw = fill(mv, n)
@@ -485,13 +113,13 @@ function initialize_sbm_model(config::Config)
         kh₀ = kh₀,
         f = sbm.f,
         zi = sbm.zi,
-        soilthickness = soilthickness,
-        θₑ = θₛ .- θᵣ,
+        soilthickness = sbm.soilthickness,
+        θₑ = sbm.θₛ .- sbm.θᵣ,
         Δt = 1.0,
         βₗ = βₗ,
         dl = dl .* 1000.0,
         dw = dw .* 1000.0,
-        ssf = ((kh₀ .* βₗ) ./ sbm.f) .* (exp.(-sbm.f .* sbm.zi) - exp.(-sbm.f .* soilthickness)) .* dw .*1000.0,
+        ssf = ((kh₀ .* βₗ) ./ sbm.f) .* (exp.(-sbm.f .* sbm.zi) - exp.(-sbm.f .* sbm.soilthickness)) .* dw .*1000.0,
         wb_pit = pits[inds],
     )
 
@@ -574,7 +202,7 @@ function initialize_sbm_model(config::Config)
         y_nc,
         dims_xy,
         nc,
-        maxlayers = maxlayers,
+        maxlayers = sbm.maxlayers,
     )
 
     # for each domain save the directed acyclic graph, the traversion order,
