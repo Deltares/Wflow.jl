0c78d432879fe2e144350e148adc86ea09445b0f
verseve <willem.vanverseveld@gmail.com>
2020-11-20 15:53:53 +0100

    update bmi + api section toml - added couple of functions - to get the var names the model can exchange

--- src/bmi.jl
+++ src/bmi.jl
@@ -27,3 +27,33 @@ function BMI.update_until(model::Model, time::Float64)
     end
     return model
 end
+
+function BMI.finalize(model::Model)
+    @unpack config, writer, clock = model
+    rewind!(clock)
+    write_netcdf_timestep(model, writer.state_dataset, writer.state_parameters)
+    reset_clock!(model.clock, config)
+    close_files(model, delete_output = false)
+end
+
+function BMI.get_component_name(model::Model)
+    @unpack config = model
+    return config.model.type
+end
+
+function BMI.get_input_var_names(model::Model)
+    @unpack config = model
+    if haskey(config,"API")
+        var_names = Vector{String}()
+        for c in config.API.components
+            append!(var_names, collect(string.(c,".", fieldnames(typeof(param(model, c))))))
+        end
+        return var_names
+    else
+        @warn("TOML file does not contain section [API] to extract model var names")
+    end
+end
+
+function BMI.get_output_var_names(model::Model)
+    BMI.get_input_var_names(model)
+end
