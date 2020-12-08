6b789ffbffea181f775bdf13d999b86c53fe8c15
verseve <willem.vanverseveld@gmail.com>
2020-12-08 12:26:14 +0100

    overland flow and no upstream nodes support rivers with no upstream nodes was fixed for lateral subsurface flow, now also fixed for overland flow.

--- src/flow.jl
+++ src/flow.jl
@@ -74,22 +74,24 @@ function update(
     for _ = 1:its
         for v in order
             upstream_nodes = inneighbors(graph, v)
+            upstream_excl_pits = filter(i -> !sf.wb_pit[i], upstream_nodes)
             # for overland flow frac_toriver and river cells need to be defined
             if (frac_toriver !== nothing) && (river !== nothing)
                 # for a river cell without a reservoir or lake (wb_pit is false) part of the upstream surface flow
                 # goes to the river (frac_toriver) and part goes to the surface flow reservoir (1.0 - frac_toriver)
                 # upstream nodes with a reservoir or lake are excluded
                 if river[v] && !sf.wb_pit[v] && sf.width[v] > 0.0
-                    qin = sum(
-                        sf.q[i] * (1.0 - frac_toriver[i])
-                        for i in upstream_nodes if !sf.wb_pit[i]
+                    qin = sum_at(
+                        i -> sf.q[i] * (1.0 - frac_toriver[i]),
+                        upstream_excl_pits,
+                        eltype(sf.q),
                     )
                     # TODO: if in a loop should also take average!!!
-                    sf.to_river[v] += sum(
-                        sf.q[i] * frac_toriver[i] for i in upstream_nodes if !sf.wb_pit[i]
+                    sf.to_river[v] += sum_at(
+                        i -> sf.q[i] * frac_toriver[i],
+                        upstream_excl_pits,
+                        eltype(sf.to_river),
                     )
-                    # for a river cell with a reservoir or lake (wb_pit is true) all upstream surface flow goes
-                    # to the river.
                 elseif river[v] && sf.wb_pit[v] && sf.width[v] == 0.0
                     sf.to_river[v]  += sum_at(sf.q, upstream_nodes)
                     qin = 0.0
