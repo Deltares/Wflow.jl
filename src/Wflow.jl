0a34bfb8e4da5fd1ee66572a9a869616abed818d
Martijn Visser <mgvisser@gmail.com>
2021-01-03 21:07:41 +0100

    add ProgressLogging to simulation loop
    This makes it run the simulation loop over the range starttime:dt:endtime. This is slightly different than the `is_finished` used before, hence two test values need to be updated.
    
    Besides the progressmeter basic model information is shown before the run. We still suppress all logging output for testing.
    
    Note that by default the progress bar is not shown. This only happens in VS Code / Juno or when using the TerminalLoggers logger. We will use the logger in the wflow_cli app.

--- src/Wflow.jl
+++ src/Wflow.jl
@@ -12,6 +12,7 @@ using BasicModelInterface
 using FieldMetadata
 using Parameters
 using DelimitedFiles
+using ProgressLogging
 
 @metadata get_units "mm"
 
@@ -105,9 +106,15 @@ function run_simulation(model::Model; close_files = true)
     # in the case of sbm_gwf it's currently a bit hard to use dispatch
     update_func = config.model.type == "sbm_gwf" ? update_sbm_gwf : update
 
-    while true
+    # determine timesteps to run
+    starttime = config.starttime::DateTime
+    endtime = config.endtime::DateTime
+    Δt = clock.Δt
+    times = range(starttime, endtime, step=clock.Δt)
+
+    @info "Run information" starttime Δt endtime
+    @progress for time in times
         model = update_func(model)
-        is_finished(clock, config) && break
     end
 
     # write output state NetCDF
@@ -127,7 +134,7 @@ function run_simulation(model::Model; close_files = true)
 end
 
 function run_simulation()
-    usage = "Usage: julia -e 'using Wflow; Wflow.run_simulation()` 'path/to/config.toml'"
+    usage = "Usage: julia -e 'using Wflow; Wflow.run_simulation()' 'path/to/config.toml'"
     n = length(ARGS)
     if n != 1
         throw(ArgumentError(usage))
