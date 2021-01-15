fb6eab0ebf38a3d265f6db395cb48a6e6fdc7ad7
verseve <willem.vanverseveld@gmail.com>
2021-01-15 13:08:01 +0100

    remove empty ConstantHead not correctly initialised and not used

--- src/sbm_gwf_model.jl
+++ src/sbm_gwf_model.jl
@@ -227,9 +227,6 @@ function initialize_sbm_gwf_model(config::Config)
         )
         index_constanthead = filter(i -> !isequal(constanthead[i], mv), 1:n)
         constant_head = ConstantHead(constanthead[index_constanthead], index_constanthead)
-
-    else
-        constant_head = ConstantHead[]
     end
 
     conductivity = ncread(
