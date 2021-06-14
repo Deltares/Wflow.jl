75c0bbb118a41420e5be63cb486ab710124666c6
Martijn Visser <mgvisser@gmail.com>
2021-06-14 21:13:48 +0200

    set restart time correctly

--- test/run.jl
+++ test/run.jl
@@ -4,7 +4,7 @@ using Dates
 tomlpath = joinpath(@__DIR__, "sbm_simple.toml")
 Wflow.run(tomlpath)
 
-# test whether restarted runs get the same results as continuus ones, i.e. state is captured
+# test whether restarted runs get the same results as continuous ones, i.e. state is captured
 tomlpath = joinpath(@__DIR__, "sbm_config.toml")
 config = Wflow.Config(tomlpath)
 
@@ -28,11 +28,24 @@ model = Wflow.initialize_sbm_model(config)
 Wflow.run(model)
 
 # second half of January, warm start
-config.starttime = DateTime("2000-01-15T00:00:00")
+config.starttime = DateTime("2000-01-16T00:00:00")
 config.endtime   = DateTime("2000-02-01T00:00:00")
 config.model.reinit = false  # cold start
+config.fews_run = false
 config.state.path_input = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january-1of2.nc")
 config.state.path_output = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january-2of2.nc")
 config.output.path = joinpath(dirname(tomlpath), "data/state-test/output-moselle-january-2of2.nc")
+model = Wflow.initialize_sbm_model(config)
+Wflow.run(model)
+
+# second half of January, warm start, fews_run set to true, and starttime set one day earlier
+# to match endtime of part 1
+config.starttime = DateTime("2000-01-15T00:00:00")
+config.endtime   = DateTime("2000-02-01T00:00:00")
+config.model.reinit = false  # cold start
+config.fews_run = true
+config.state.path_input = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january-1of2.nc")
+config.state.path_output = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january-2of2-fews_run.nc")
+config.output.path = joinpath(dirname(tomlpath), "data/state-test/output-moselle-january-2of2-fews_run.nc")
 model = Wflow.initialize_sbm_model(config)
 Wflow.run(model)
