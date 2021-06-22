d4cdfa0f339a45acae9da7561a9fa680be7b45ec
Martijn Visser <mgvisser@gmail.com>
2021-06-22 22:12:17 +0200

    test end state of restarted vs continuous run

--- test/run.jl
+++ test/run.jl
@@ -1,5 +1,7 @@
+using Test
 using Wflow
 using Dates
+using NCDatasets
 
 tomlpath = joinpath(@__DIR__, "sbm_simple.toml")
 Wflow.run(tomlpath)
@@ -49,3 +51,20 @@ config.state.path_output = joinpath(dirname(tomlpath), "data/state-test/outstate
 config.output.path = joinpath(dirname(tomlpath), "data/state-test/output-moselle-january-2of2-fews_run.nc")
 model = Wflow.initialize_sbm_model(config)
 Wflow.run(model)
+
+# verify that there are minimal differences in the end state of the two runs
+endstate_one_run_path = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january.nc")
+endstate_restart_path = joinpath(dirname(tomlpath), "data/state-test/outstates-moselle-january-2of2.nc")
+endstate_one_run = NCDataset(endstate_one_run_path)
+endstate_restart = NCDataset(endstate_restart_path)
+
+varnames = setdiff(keys(endstate_restart), keys(endstate_restart.dim))
+@testset "restart" begin
+    @test length(varnames) > 10
+    for varname in varnames
+        a = endstate_one_run[varname][:]
+        b = endstate_restart[varname][:]
+        maxdiff = maximum(abs.(skipmissing(b - a)))
+        @test maxdiff < 1e-9
+    end
+end
