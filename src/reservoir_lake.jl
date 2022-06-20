fbdbb76a7ac55b99a29eae453f6c5133ff74f5c4
verseve <willem.vanverseveld@gmail.com>
2022-06-20 16:59:46 +0200

    info log message for lakes add path of CSV file with rating curve or storage curve to logging

--- src/reservoir_lake.jl
+++ src/reservoir_lake.jl
@@ -374,11 +374,13 @@ function initialize_natural_lake(config, nc, inds_riv, nriv, pits, Δt)
 
         if lake_storfunc[i] == 2
             csv_path = joinpath(path, "lake_sh_$lakeloc.csv")
+            @info("read a storage curve from CSV file $csv_path, for lake location $lakeloc")
             sh[i] = read_sh_csv(csv_path)
         end
 
         if lake_outflowfunc[i] == 1
             csv_path = joinpath(path, "lake_hq_$lakeloc.csv")
+            @info("read a rating curve from CSV file $csv_path, for lake location $lakeloc")
             hq[i] = read_hq_csv(csv_path)
         end
 
