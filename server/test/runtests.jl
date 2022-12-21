b8ff635108e56daca7928d72e1b8c55e9ca5fd8a
verseve <willem.vanverseveld@gmail.com>
2022-12-21 16:51:15 +0100

    run tests from runtests.jl

---
+++ server/test/runtests.jl
@@ -0,0 +1,14 @@
+using ZMQ
+using ZMQ
+using JSON3
+using StructTypes
+using Wflow
+using Statistics
+using Logging
+using Test
+
+with_logger(NullLogger()) do
+    @testset "Test Wflow client server Wflow (BMI and ZMQ)" begin
+        include("client.jl")
+    end
+end
