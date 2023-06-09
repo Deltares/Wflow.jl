9a56ef586713c282894ed23f04d99a5ec458da1b
JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>
2023-06-09 16:52:50 +0200

    Add `sum` to reducer options

--- src/io.jl
+++ src/io.jl
@@ -1214,6 +1214,7 @@ function reducerfunction(reducer::AbstractString)
         "first" => first,
         "last" => last,
         "only" => only,
+        "sum" => sum,
     )
     f = get(functionmap, reducer, nothing)
     isnothing(f) && error("unknown reducer")
