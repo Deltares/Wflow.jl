03a7fbeb44d284f707ef66d20697ad3270475498
Willem van Verseveld <willem.vanverseveld@gmail.com>
2024-02-20 12:58:08 +0100

    Fix import for tests

--- server/test/runtests.jl
+++ server/test/runtests.jl
@@ -4,12 +4,12 @@ import StructTypes
 import Wflow
 import WflowServer
 import Statistics: mean
-import Logging: NullLogger
-import Test: @testset
+import Logging
+import Test: @testset, @test
 
 # The tests depend on wflow_sbm model files (Moselle), which are downloaded as part of the
 # Wflow.jl tests, so these need to run first.
-with_logger(NullLogger()) do
+Logging.with_logger(Logging.NullLogger()) do
     @testset "Test client server Wflow ZMQ Server" begin
         include("client.jl")
     end
