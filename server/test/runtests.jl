c9fd33053cf31c6bb28dfcd9068f49d7e5fa6ef7
Willem van Verseveld <willem.vanverseveld@gmail.com>
2024-02-20 15:12:49 +0100

    Make testing work from `Pkg` - Project.toml needs uuid - Logging dep was missing

--- server/test/runtests.jl
+++ server/test/runtests.jl
@@ -4,12 +4,12 @@ import StructTypes
 import Wflow
 import WflowServer
 import Statistics: mean
-import Logging
+import Logging: with_logger, NullLogger
 import Test: @testset, @test
 
 # The tests depend on wflow_sbm model files (Moselle), which are downloaded as part of the
 # Wflow.jl tests, so these need to run first.
-Logging.with_logger(Logging.NullLogger()) do
+with_logger(NullLogger()) do
     @testset "Test client server Wflow ZMQ Server" begin
         include("client.jl")
     end
