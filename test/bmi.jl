6b8a5d0825662fe9e035d13f6747a8b7a0f122f4
Willem van Verseveld <willem.vanverseveld@gmail.com>
2024-04-17 16:16:38 +0200

    Fix `BMI.update_until` (`InexactError: Int64`) (#387)
    * Fix `BMI.update_until` (`InexactError: Int64`)
    
    * Update changelog
    
    * Improve error handling of `BMI.update_until`
    
    * Address review comments
    - Use `divrem` for computing `steps` in `BMI.update_until` function. The remainder `rem` is used to throw an error message.
    - take the for loop in function `BMI.update_until` out of the conditional block.
    
    Co-authored-by: Carlos Fernando Baptista <cfd.baptista@gmail.com>
    
    * Update and add tests `BMI.update_until`
    
    * Small update tests `BMI.update_until`
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Carlos Fernando Baptista <cfd.baptista@gmail.com>

--- test/bmi.jl
+++ test/bmi.jl
@@ -117,8 +117,12 @@ tomlpath = joinpath(@__DIR__, "sbm_config.toml")
 
         @testset "update until and finalize" begin
             time = BMI.get_current_time(model) + 2 * BMI.get_time_step(model)
-            @test_logs (:info, "update model until 9.470304e8")
             model = BMI.update_until(model, time)
+            @test model.clock.iteration == 3
+            time_off = BMI.get_current_time(model) + 1 * BMI.get_time_step(model) + 1e-06
+            @test_throws ErrorException model = BMI.update_until(model, time_off)
+            @test_throws ErrorException model =
+                BMI.update_until(model, time - BMI.get_time_step(model))
             BMI.finalize(model)
         end
 
