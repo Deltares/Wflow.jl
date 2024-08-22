b529aed57a1701a31af7702e2030c6e8ef122a35
Willem van Verseveld <willem.vanverseveld@gmail.com>
2024-08-22 11:38:38 +0200

    Reduce allocations related to water demand in `SBM` Defining water demand fields in the SBM struct of Union type (e.g.  Union{Paddy,Nothing}) results in significant more allocations and slower run times (if statement with !isnothing()). This has been fixed by Including these fields as type parameters.

--- src/sbm.jl
+++ src/sbm.jl
@@ -1,4 +1,14 @@
-@get_units @exchange @grid_type @grid_location @with_kw struct SBM{T,N,M}
+@get_units @exchange @grid_type @grid_location @with_kw struct SBM{
+    T,
+    N,
+    M,
+    P<:Union{Paddy,Nothing},
+    NP<:Union{NonPaddy,Nothing},
+    D<:Union{NonIrrigationDemand,Nothing},
+    L<:Union{NonIrrigationDemand,Nothing},
+    I<:Union{NonIrrigationDemand,Nothing},
+    A<:Union{AllocationLand,Nothing},
+}
     # Model time step [s]
     dt::T | "s" | 0 | "none" | "none"
     # Maximum number of soil layers
@@ -225,15 +235,15 @@
     # Total water storage (excluding floodplain volume, lakes and reservoirs) [mm]
     total_storage::Vector{T} | "mm"
     # Water demand structs (of arrays)
-    paddy::Union{Paddy,Nothing} | "-" | 0
-    nonpaddy::Union{NonPaddy,Nothing} | "-" | 0
-    domestic::Union{NonIrrigationDemand,Nothing} | "-" | 0
-    livestock::Union{NonIrrigationDemand,Nothing} | "-" | 0
-    industry::Union{NonIrrigationDemand,Nothing} | "-" | 0
-    allocation::Union{AllocationLand,Nothing} | "-" | 0
+    paddy::P | "-" | 0
+    nonpaddy::NP | "-" | 0
+    domestic::D | "-" | 0
+    livestock::L | "-" | 0
+    industry::I | "-" | 0
+    allocation::A | "-" | 0
 
 
-    function SBM{T,N,M}(args...) where {T,N,M}
+    function SBM{T,N,M,P,NP,D,L,I,A}(args...) where {T,N,M,P,NP,D,L,I,A}
         equal_size_vectors(args)
         return new(args...)
     end
