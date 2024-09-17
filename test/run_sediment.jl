02883418a571effc3d6d5366bc8433d3fc9e9e44
hboisgon <helene.boisgontier@deltares.nl>
2024-09-17 14:41:53 +0800

    Working tests for soil erosion

--- test/run_sediment.jl
+++ test/run_sediment.jl
@@ -11,33 +11,41 @@ model = Wflow.run_timestep(model)
 @testset "first timestep sediment model (vertical)" begin
     eros = model.vertical
 
+    @test eros.hydrometeo_forcing.precipitation[1] ≈ 4.086122035980225f0
     @test eros.hydrometeo_forcing.q_land[1] ≈ 0.0f0
-    @test eros.overland_flow_erosion.parameters.usle_k[1] ≈ 0.026510488241910934
-    @test eros.overland_flow_erosion.variables.amount[1] ≈ 0.9f0
+    @test eros.overland_flow_erosion.parameters.usle_k[1] ≈ 0.026510488241910934f0
+    @test eros.overland_flow_erosion.parameters.usle_c[1] ≈ 0.014194443821907043f0
+    @test eros.overland_flow_erosion.parameters.answers_k[1] ≈ 0.9f0
+    @test eros.overland_flow_erosion.variables.amount[1] ≈ 0.0f0
+    @test eros.rainfall_erosion.variables.amount[1] ≈ 0.00027245577922893746f0
     @test model.clock.iteration == 1
     #@test mean(eros.leaf_area_index) ≈ 1.7120018886212223f0
     #@test eros.dmsand[1] == 200.0f0
     #@test eros.dmlagg[1] == 500.0f0
     #@test mean(eros.interception) ≈ 0.4767846753916875f0
-    @test mean(eros.soil_erosion.variables.amount) ≈ 0.008596682196335555f0
+    @test mean(eros.overland_flow_erosion.variables.amount) ≈ 0.00861079076689589f0
+    @test mean(eros.rainfall_erosion.variables.amount) ≈ 0.00016326203201620437f0
+    @test mean(eros.soil_erosion.variables.amount) ≈ 0.008774052798912092f0
 end
 
-# # run the second timestep
-# model = Wflow.run_timestep(model)
+# run the second timestep
+model = Wflow.run_timestep(model)
 
-# @testset "second timestep sediment model (vertical)" begin
-#     eros = model.vertical
+@testset "second timestep sediment model (vertical)" begin
+    eros = model.vertical
 
-#     @test mean(eros.soil_erosion.variables.amount) ≈ 0.07601393657280235f0
-#     @test mean(eros.soil_erosion.variables.clay) ≈ 0.0022388720384961766f0
-#     @test mean(eros.soil_erosion.variables.sand) ≈ 0.02572046244407882f0
-#     @test mean(eros.soil_erosion.variables.lagg) ≈ 0.022126541806118796f0
-#     #@test mean(eros.TCsed) == 0.0
-#     #@test mean(eros.TCsilt) ≈ 1.0988158364353527f6
-#     #@test mean(eros.TCsand) ≈ 1.0987090622888755f6
-#     #@test mean(eros.TCclay) ≈ 1.0992655197016734f6
-#     #@test mean(eros.TCsilt) ≈ 1.0988158364353527f6
-# end
+    @test mean(eros.soil_erosion.variables.amount) ≈ 0.07765800489746684f0
+    @test mean(eros.soil_erosion.variables.clay) ≈ 0.002287480354866626f0
+    @test mean(eros.soil_erosion.variables.silt) ≈ 0.0036164773521184896f0
+    @test mean(eros.soil_erosion.variables.sand) ≈ 0.026301393837924607f0
+    @test mean(eros.soil_erosion.variables.lagg) ≈ 0.022577957752547836f0
+    @test mean(eros.soil_erosion.variables.sagg) ≈ 0.022874695590802723f0
+    #@test mean(eros.TCsed) == 0.0
+    #@test mean(eros.TCsilt) ≈ 1.0988158364353527f6
+    #@test mean(eros.TCsand) ≈ 1.0987090622888755f6
+    #@test mean(eros.TCclay) ≈ 1.0992655197016734f6
+    #@test mean(eros.TCsilt) ≈ 1.0988158364353527f6
+end
 
 # @testset "second timestep sediment model (lateral)" begin
 #     lat = model.lateral
