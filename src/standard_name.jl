937f42b1f5816de34e9d526ddb070f7d74b11ca8
Willem van Verseveld <willem.vanverseveld@gmail.com>
2025-01-09 15:11:05 +0100

    Add more (output) variables to `sbm_standard_name_map`

--- src/standard_name.jl
+++ src/standard_name.jl
@@ -1,14 +1,23 @@
-
 const sbm_standard_name_map = Dict{String, ComposedFunction}(
     "atmosphere_water__precipitation_volume_flux" =>
         @optic(_.vertical.atmospheric_forcing.precipitation),
     "land_surface_water__potential_evaporation_volume_flux" =>
         @optic(_.vertical.atmospheric_forcing.potential_evaporation),
+    "land_surface__evapotranspiration_volume_flux" =>
+        @optic(_.vertical.soil.variables.actevap),
+    "land_water__total_storage" => @optic(_.vertical.soil.variables.total_storage),
     "atmosphere_air__temperature" => @optic(_.vertical.atmospheric_forcing.temperature),
     "vegetation__leaf-area_index" =>
         @optic(_.vertical.vegetation_parameter_set.leaf_area_index),
     "vegetation_canopy_water__storage" =>
         @optic(_.vertical.interception.variables.canopy_storage),
+    "vegetation_canopy_water__stemflow_volume_flux" =>
+        @optic(_.vertical.interception.variables.stemflow),
+    "vegetation_canopy_water__throughfall_volume_flux" =>
+        @optic(_.vertical.interception.variables.throughfall),
+    "snowpack__liquid-equivalent_depth" => @optic(_.vertical.snow.variables.swe),
+    "snowpack_meltwater__volume_flux" => @optic(_.vertical.snow.variables.snow_melt),
+    "snowpack_water__runoff_volume_flux" => @optic(_.vertical.snow.variables.runoff),
     "river_water__volume_inflow_rate" =>
         @optic(_.lateral.river.boundary_conditions.inflow),
     "river_water__volume_flow_rate" => @optic(_.lateral.river.variables.q),
@@ -17,16 +26,52 @@ const sbm_standard_name_map = Dict{String, ComposedFunction}(
     "river_water__depth" => @optic(_.lateral.river.variables.h),
     "river_water__time_average_of_depth" => @optic(_.lateral.river.variables.h_av),
     "river_water__volume" => @optic(_.lateral.river.variables.volume),
+    "land_surface_water_use~abstraction__volume_flux" =>
+        @optic(_.lateral.river.allocation.variables.act_surfacewater_abst),
     "floodplain_water__volume" => @optic(_.lateral.river.floodplain.variables.volume),
     "floodplain_water__depth" => @optic(_.lateral.river.floodplain.variables.h),
     "floodplain_water__volume_flow_rate" =>
         @optic(_.lateral.river.floodplain.variables.q),
+    "floodplain_water__time_average_of_volume_flow_rate" =>
+        @optic(_.lateral.river.floodplain.variables.q_av),
     "reservoir_water__volume" =>
         @optic(_.lateral.river.boundary_conditions.reservoir.variables.volume),
+    "reservoir_water~outgoing__time_average_of_volume_flow_rate" =>
+        @optic(_.lateral.river.boundary_conditions.reservoir.variables.outflow_av),
+    "reservoir_water__volume_fraction" =>
+        @optic(_.lateral.river.boundary_conditions.reservoir.variables.percfull),
+    "lake_water__volume" =>
+        @optic(_.lateral.river.boundary_conditions.lake.variables.storage),
+    "lake_water~outgoing__time_average_of_volume_flow_rate" =>
+        @optic(_.lateral.river.boundary_conditions.lake.variables.outflow_av),
+    "soil_water__infiltration_volume_flux" =>
+        @optic(_.vertical.soil.variables.actinfilt),
+    "soil_water__transpiration_volume_flux" =>
+        @optic(_.vertical.soil.variables.transpiration),
+    "soil_surface_water__runoff_volume_flux" =>
+        @optic(_.vertical.soil.variables.runoff),
+    "soil_surface_water__net_runoff_volume_flux" =>
+        @optic(_.vertical.soil.variables.net_runoff),
+    "soil_water__volume_fraction-per-soil_layer" =>
+        @optic(_.vertical.soil.variables.vwc),
+    "soil_water__volume_percentage-per-soil_layer" =>
+        @optic(_.vertical.soil.variables.vwc_perc),
+    "soil_water_root-zone__volume_fraction" =>
+        @optic(_.vertical.soil.variables.vwc_root),
+    "soil_water_root-zone__volume_percentage" =>
+        @optic(_.vertical.soil.variables.vwc_percroot),
     "soil_water_sat-zone_top__recharge_volume_flux" =>
         @optic(_.vertical.soil.variables.recharge),
     "soil_water_unsat-zone__depth-per-soil_layer" =>
         @optic(_.vertical.soil.variables.ustorelayerdepth),
+    "soil_water_sat-zone_top__capillary_volume_flux" =>
+        @optic(_.vertical.soil.variables.actcapflux),
+    "soil_water_sat-zone_top__recharge_volume_flux" =>
+        @optic(_.vertical.soil.variables.transfer),
+    "soil_water_sat-zone_top__net_recharge_volume_flux" =>
+        @optic(_.vertical.soil.variables.recharge),
+    "soil_water_sat-zone_bottom__leakage_volume_flux" =>
+        @optic(_.vertical.soil.variables.actleakage),
     "soil_water_sat-zone__depth" => @optic(_.vertical.soil.variables.satwaterdepth),
     "soil_water_sat-zone_top__depth" => @optic(_.vertical.soil.variables.zi),
     "soil_surface__temperature" => @optic(_.vertical.soil.variables.tsoil),
@@ -34,7 +79,11 @@ const sbm_standard_name_map = Dict{String, ComposedFunction}(
     "snowpack~liquid__depth" => @optic(_.vertical.snow.variables.snow_water),
     "snowpack~dry__leq-depth" => @optic(_.vertical.snow.variables.snow_storage),
     "glacier_ice__leq-volume" => @optic(_.vertical.glacier.variables.glacier_store),
+    "glacier_ice__melt_volume_flux" =>
+        @optic(_.vertical.glacier.variables.glacier_melt),
     "land_surface_water__volume_flow_rate" => @optic(_.lateral.land.variables.q),
+    "land_surface_water__time_average_of_volume_flow_rate" =>
+        @optic(_.lateral.land.variables.q_av),
     "land_surface_water__depth" => @optic(_.lateral.land.variables.h),
     "land_surface_water__time_average_of_depth" =>
         @optic(_.lateral.land.variables.h_av),
@@ -58,13 +107,21 @@ const sbm_standard_name_map = Dict{String, ComposedFunction}(
         @optic(_.vertical.demand.livestock.demand.demand_net),
     "land~irrigated-paddy__irrigation_trigger_flag" =>
         @optic(_.vertical.demand.paddy.parameters.irrigation_trigger),
+    "land~irrigated-paddy__gross_water_demand_flux" =>
+        @optic(_.vertical.demand.paddy.variables.demand_gross),
     "land~irrigated-non-paddy__irrigation_trigger_flag" =>
         @optic(_.vertical.demand.nonpaddy.parameters.irrigation_trigger),
+    "land~irrigated-non-paddy__gross_water_demand_flux" =>
+        @optic(_.vertical.demand.nonpaddy.variables.demand_gross),
     "land~irrigated__allocated_water_volume_flux" =>
         @optic(_.vertical.allocation.variables.irri_alloc),
+    "land~non-irrigated__allocated_water_volume_flux" =>
+        @optic(_.vertical.allocation.variables.nonirri_alloc),
+    "subsurface_water_use~abstraction__volume_flux" =>
+        @optic(_.vertical.allocation.variables.act_groundwater_abst),
     "subsurface_water__hydraulic_head" =>
         @optic(_.lateral.subsurface.flow.aquifer.variables.head),
-    "subsurface_water_sat-zone_top__net-recharge_volume_flux" =>
+    "subsurface_water_sat-zone_top__net_recharge_volume_flux" =>
         @optic(_.lateral.subsurface.recharge.variables.flux),
     "land_drain_water~to-subsurface__volume_flow_rate" =>
         @optic(_.lateral.subsurface.drain.variables.flux),
