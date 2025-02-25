c37d13f84d0da263408a5db7bd532ee3e08ad1b1
Willem van Verseveld <willem.vanverseveld@gmail.com>
2025-02-25 13:21:33 +0100

    BMI exchange and metadata (#552)
    Fixes issues #391 and #432.
    
    Co-authored-by: JoostBuitink <44062204+JoostBuitink@users.noreply.github.com>

--- src/utils.jl
+++ src/utils.jl
@@ -188,7 +188,7 @@ function set_states!(instate_path, model; type = nothing, dimname = nothing)
                     end
                 end
                 # set state in model object
-                lens = standard_name_map(land)[state]
+                lens = standard_name_map(land)[state].lens
                 lens(model) .= svectorscopy(A, Val{size(A)[1]}())
                 # 3 dims (x,y,time)
             elseif dims == 3
@@ -202,7 +202,7 @@ function set_states!(instate_path, model; type = nothing, dimname = nothing)
                     end
                 end
                 # set state in model object, only set active cells ([1:n]) (ignore boundary conditions/ghost points)
-                lens = standard_name_map(land)[state]
+                lens = standard_name_map(land)[state].lens
                 lens(model)[1:n] .= A
             else
                 error(
