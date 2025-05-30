c148b790789c414ad98b10902b63f85c83773d43
deltamarnix <deltamarnix@users.noreply.github.com>
2025-05-30 18:27:08 +0200

    Add pixi task to genenate SBOM
    Generate an SPDX standardized SBOM with all dependencies used by Wflow.jl
    
    Still misses Julia and its depending third parties.
    See https://github.com/JuliaLang/julia/blob/master/THIRDPARTY.md
    
    Opened an issue to add even more information by PkgToSoftwareBOM:
    https://github.com/SamuraiAku/PkgToSoftwareBOM.jl/issues/38
    
    This task could be added to when the project.toml file is being updated
    in GitHub actions.

---
+++ Wflow/utils/generate-sbom.jl
@@ -0,0 +1,28 @@
+using PkgToSoftwareBOM
+using UUIDs
+using Pkg
+
+wflowLicense = SpdxLicenseExpressionV2("MIT")
+organization = SpdxCreatorV2("Organization", "Deltares", "software@deltares.nl")
+packageInstructions = spdxPackageInstructions(;
+    spdxfile_toexclude = ["Wflow.spdx.json"],
+    originator = organization,
+    declaredLicense = wflowLicense,
+    copyright = "Copyright (c) 2025 Deltares <software@deltares.nl>",
+    name = "Wflow",
+)
+
+dependencies =  filter(p-> !(p.first in ["PkgToSoftwareBOM", "SPDX"]), Pkg.project().dependencies);
+spdxData = spdxCreationData(;
+    Name = "Wflow.jl",
+    Creators = [organization],
+    NamespaceURL = "https://github.com/Deltares/Wflow.jl/Wflow.spdx.json",
+    rootpackages = dependencies,
+    find_artifactsource = true,
+    packageInstructions = Dict{UUID, spdxPackageInstructions}(
+        Pkg.project().uuid => packageInstructions,
+    ),
+)
+
+sbom = generateSPDX(spdxData)
+writespdx(sbom, "Wflow.spdx.json")
