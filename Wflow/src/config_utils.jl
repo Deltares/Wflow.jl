0206dbe88d1285e60a9ce850b413ce594ddefe74
Bart de Koning <bart123-4@hotmail.com>
2025-09-25 15:11:46 +0200

    Add backticks to names and values in info message

--- Wflow/src/config_utils.jl
+++ Wflow/src/config_utils.jl
@@ -126,9 +126,9 @@ function variable_info(var::InputEntry)
     (; layer, scale, offset) = var
 
     for (i, (scale, offset)) in enumerate(zip(scale, offset))
-        msg = "NetCDF parameter $(variable_name(var)) is modified"
+        msg = "NetCDF parameter `$(variable_name(var))` is modified"
         if !isone(scale) || !iszero(offset)
-            msg *= "with scale $scale and offset $offset"
+            msg *= "with scale `$scale` and offset `$offset`"
         end
         !isnothing(layer) && (msg *= "at index $(layer[i])")
         @info "$msg."
