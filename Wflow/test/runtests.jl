063ca16a6c99a3e861389f84b974b9fe549945cc
Bart de Koning <bart123-4@hotmail.com>
2025-09-25 18:26:37 +0200

    Run tests with TestItemRunner

--- Wflow/test/runtests.jl
+++ Wflow/test/runtests.jl
@@ -1,43 +1,9 @@
-## load test dependencies and set paths to testing data
-using Accessors
-using Dates
-using Graphs
-using NCDatasets
-using StaticArrays
-using Statistics
-using Test
-using Wflow
-using Base.MathConstants: eulergamma
-using Base.Threads
-using BasicModelInterface
-using Polynomials: Polynomials
-using DelimitedFiles
+using TestItemRunner
 using LoggingExtras
-using QuadGK
-using Aqua: Aqua
-
-const BMI = BasicModelInterface
+using Base.Threads
 
 @info "testing Wflow with" nthreads() VERSION
 
-# disable logging output during testing
 with_logger(NullLogger()) do
-    ## run all tests
-    @testset "Wflow.jl" begin
-        include("routing_process.jl")
-        include("io.jl")
-        include("land_process.jl")
-        include("reservoir.jl")
-        include("run_sbm.jl")
-        include("run_sbm_piave.jl")
-        include("run_sbm_gwf_piave.jl")
-        include("run_sbm_gwf.jl")
-        include("run.jl")
-        include("groundwater.jl")
-        include("utils.jl")
-        include("bmi.jl")
-        include("run_sediment.jl")
-        include("subdomains.jl")
-        include("aqua.jl")
-    end
+    @run_package_tests
 end
