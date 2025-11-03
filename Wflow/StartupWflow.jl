261c1565911336e871f8ec4a2fbb514a354ad6f5
“AquaPore” <pollacco.water@gmail.com>
2025-11-03 13:05:32 +0000

    Test gitignore

---
+++ Wflow/StartupWflow.jl
@@ -0,0 +1,10 @@
+using Pkg
+# Path="sbm_config_simple.toml"
+Path="sbm_config_simple_Timoleague.toml"
+cd(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow")
+Pkg.activate(".")
+include(raw"src\Wflow.jl")
+Main.Wflow.run(Path)
+
+
+
