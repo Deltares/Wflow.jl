3771f2b8a1afdf18081701a8272507557a65be9c
“AquaPore” <pollacco.water@gmail.com>
2025-11-04 19:13:20 +0000

    parameters

--- Wflow/StartupWflow.jl
+++ Wflow/StartupWflow.jl
@@ -1,6 +1,6 @@
 using Pkg
 # Path="sbm_config_simple.toml"
-Path="sbm_config_simple_Timoleague.toml"
+Path="sbm_config_Timoleague.toml"
 cd(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow")
 Pkg.activate(".")
 include(raw"src\Wflow.jl")
