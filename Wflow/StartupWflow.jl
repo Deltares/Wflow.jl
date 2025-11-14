2829d599a91b8ab9a6e3e0469c439d8506d8aac6
“AquaPore” <pollacco.water@gmail.com>
2025-11-14 12:24:55 +0000

    dELTARES

--- Wflow/StartupWflow.jl
+++ Wflow/StartupWflow.jl
@@ -1,8 +1,7 @@
 # using Revise
 using Pkg
-# Path="sbm_config_simple.toml"
-# Path="sbm_config_Timoleague.toml"
-Path="sbm_config_Moselle.toml"
+Path="sbm_config_Timoleague.toml"
+# Path="sbm_config_Moselle.toml"
 cd(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow")
 Pkg.activate(".")
 include(raw"src\Wflow.jl")
