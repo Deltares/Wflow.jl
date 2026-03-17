bd91458b96b8dde343e32ecdb5a48e67cdc58cc5
Bart de Koning <bart123-4@hotmail.com>
2026-03-17 18:27:05 +0100

    Fix

--- Wflow/test/io.jl
+++ Wflow/test/io.jl
@@ -519,7 +519,7 @@ end
         @test isfile(toml_archive)
         @test isfile(path_log)
         lines = readlines(path_log)
-        @test count(startswith(line, "[ Info: ") for line in lines) == 59
+        @test count(startswith(line, "[ Info: ") for line in lines) == 15
         @test count(startswith(line, "┌ Debug: ") for line in lines) == 0
 
         # Another run with debug log level and a non-default path_log.
