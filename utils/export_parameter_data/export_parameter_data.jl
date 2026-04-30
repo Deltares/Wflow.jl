4cc8255870d1cafd2849e201f9cc05b8aa20a9eb
Bart de Koning <bart123-4@hotmail.com>
2026-04-30 14:37:46 +0200

    Comments addressed

--- utils/export_parameter_data/export_parameter_data.jl
+++ utils/export_parameter_data/export_parameter_data.jl
@@ -1,11 +1,12 @@
 using Wflow
 using JSON
+using OrderedCollections: OrderedDict
 
 function to_dict(standard_name_map)
-    out = Dict{String, Dict{String, Any}}()
+    out = OrderedDict{String, OrderedDict{String, Any}}()
 
     for (standard_name, metadata) in standard_name_map
-        out[standard_name] = Dict(
+        out[standard_name] = OrderedDict(
             "unit" => Wflow.to_string(metadata.unit; BMI_standard = true),
             "default" => metadata.default,
             "fill" => metadata.fill,
@@ -19,8 +20,9 @@ function to_dict(standard_name_map)
     return out
 end
 
-for (name, standard_name_map) in Wflow.STANDARD_NAME_MAPS
+for (name, standard_name_map, _) in Wflow.STANDARD_NAME_MAPS
     dict = to_dict(standard_name_map)
+    sort!(dict)
     path = normpath(@__DIR__, "$(name)_metadata.json")
     open(path, "w") do io
         print(io, JSON.json(dict, 2))
