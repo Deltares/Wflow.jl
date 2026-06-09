b0815bebee4a92b56b6df5499acc3f6cf830528c
Bart de Koning <bart123-4@hotmail.com>
2026-06-09 16:03:52 +0200

    Fix aqua test in the right place

--- server/test/aqua.jl
+++ server/test/aqua.jl
@@ -1,9 +1,5 @@
 @testitem "Aqua" begin
     import Aqua
     import SharedHydrology
-    Aqua.test_all(
-        WflowServer;
-        persistent_tasks = false,
-        piracies = (treat_as_own = [SharedHydrology],),
-    )
+    Aqua.test_all(WflowServer; persistent_tasks = false)
 end
